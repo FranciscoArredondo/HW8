e42d0e0cb0c588798287ea7e6bd8c7a2b518effd
Francisco <frarredo@ucsd.edu>
2014-05-23 17:05:14 -0700

    created the skeleton code for the Unicalc and AST testers

---
+++ ASTTester.java
@@ -0,0 +1,41 @@
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author frnk070
+ *
+ */
+public class ASTTester extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * Test method for {@link AST#toString()}.
+	 */
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link AST#eval(Map)}.
+	 */
+	public void testEval() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link AST#equals(java.lang.Object)}.
+	 */
+	public void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ UnicalcTester.java
@@ -0,0 +1,97 @@
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author frnk070
+ *
+ */
+public class UnicalcTester extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * Test method for {@link Unicalc#S()}.
+	 */
+	public void testS() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#L()}.
+	 */
+	public void testL() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#E()}.
+	 */
+	public void testE() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#P()}.
+	 */
+	public void testP() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#K()}.
+	 */
+	public void testK() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#Q()}.
+	 */
+	public void testQ() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#R()}.
+	 */
+	public void testR() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#V()}.
+	 */
+	public void testV() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#J()}.
+	 */
+	public void testJ() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#I()}.
+	 */
+	public void testI() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Unicalc#D()}.
+	 */
+	public void testD() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
